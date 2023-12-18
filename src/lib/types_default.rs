d6ea80ccec1fdcf03ece95e379308ab96a26d4c6
Chris Simpkins <chris@sourcefoundry.org>
2023-12-17 22:39:42 -0500

    types: add avif image file type

--- src/lib/types_default.rs
+++ src/lib/types_default.rs
@@ -44,6 +44,7 @@ pub(crate) const DEFAULT_TYPES: &[(&[&str], &[&str])] = &[
         "*.ascx.vb", "*.asp"
     ]),
     (&["ats"], &["*.ats", "*.dats", "*.sats", "*.hats"]),
+    (&["avif"], &["*.avif"]),
     (&["avro"], &["*.avdl", "*.avpr", "*.avsc"]),
     (&["awk"], &["*.awk"]),
     (&["bat", "batch"], &["*.bat"]),

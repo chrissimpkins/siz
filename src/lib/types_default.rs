839038375a0923e07c6c7d1b35c3efe13e9326d1
Chris Simpkins <chris@sourcefoundry.org>
2023-12-17 23:12:12 -0500

    types: add RTF format file type

--- src/lib/types_default.rs
+++ src/lib/types_default.rs
@@ -261,6 +261,7 @@ pub(crate) const DEFAULT_TYPES: &[(&[&str], &[&str])] = &[
     (&["robot"], &["*.robot"]),
     (&["rpm"], &["*.rpm"]),
     (&["rst"], &["*.rst"]),
+    (&["rtf"], &["*.rtf"]),
     (&["ruby"], &[
         // Idiomatic files
         "config.ru", "Gemfile", ".irbrc", "Rakefile",
